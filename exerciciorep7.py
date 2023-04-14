69c0c373d56890230e5f24305e99cc2fa05f5300
unknown <lorenzomeni77@gmail.com>
2023-04-14 09:48:49 -0300

    exercicios 6,7,8

---
+++ exerciciorep7.py
@@ -0,0 +1,12 @@
+numeros = []
+for num in range(1, 6):
+    numeros.append(int(input("Digite um numero: ")))
+
+maiorNumero = numeros[0]
+
+cont = 1
+while cont < len(numeros):
+    if numeros[cont] > maiorNumero:
+        maiorNumero = numeros[cont]
+    cont = cont + 1
+print(' maior numero: '+ str (maiorNumero))
---
+++ exerciciorep8.py
@@ -0,0 +1,8 @@
+numero1=float(input("digite o 1º numero: "))
+numero2=float(input("digite o 2º numero: "))
+numero3=float(input("digite o 3º numero: "))
+numero4=float(input("digite o 4º numero: "))
+numero5=float(input("digite o 5º numero: "))
+soma=numero1+numero2+numero3+numero4+numero5
+print("soma: ",soma,)
+print("média: ",soma/5)
